d92171666b4acedfa6f74d27ce4dd460f36de100
Victor Efobi <victorefobi1@gmail.com>
2023-02-18 15:50:05 -0800

    Task completed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a c program that prints
+ * to the terminal.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
