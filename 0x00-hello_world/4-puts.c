d2db8dc7c4b1cfa853463ddf2a6e0386940045d2
Victor Efobi <victorefobi1@gmail.com>
2023-02-19 14:27:00 -0800

    Task completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: a c program that prints
- * to the terminal.
+ * Description: A c program that prints with put
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-Readme
+coding in c is fun, my github directory for Alx C tasks
