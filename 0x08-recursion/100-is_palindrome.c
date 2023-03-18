1ee027e3f8642de1acef02fc4d2a05cc5b5538bc
Victor Efobi <victorefobi1@gmail.com>
2023-03-18 09:29:30 -0700

    Advanced Task

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int is_palindrome - checks if number is palindrome.
+ * @s: string.
+ *
+ * Return: 1 if palindrome.
+ *         0 if not palindrome.
+ */
+
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+
+	if (len <= 1)
+	{
+		return (1);
+	}
+	if (s != s[len - 1])
+	{
+		return (0);
+	}
+	s[len - 1] = '\0';
+	return (is_palindrome(s + 1));
+}
