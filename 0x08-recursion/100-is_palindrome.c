addf3d5b6c953fe37fd764ecc3b52ec3c68a14bd
Victor Efobi <victorefobi1@gmail.com>
2023-03-20 06:51:01 -0700

    Advanced Tasks

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,6 +16,7 @@ int check_palindrome(char *s, int left, int right);
 int is_palindrome(char *s)
 {
 	int len = strlen(s);
+
 	return (check_palindrome(s, 0, len - 1));
 }
 
@@ -35,5 +36,7 @@ int check_palindrome(char *s, int left, int right)
 		return (1);
 	else if (s[left] == s[right])
 		return (check_palindrome(s, left + 1, right - 1));
-	else return(0);
+
+	else
+		return (0);
 }
