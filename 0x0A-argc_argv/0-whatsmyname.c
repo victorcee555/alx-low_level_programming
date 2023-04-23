b8b473dc764cb2eb051e76b3a3514e7822b629f1
Victor Efobi <victorefobi1@gmail.com>
2023-04-23 13:23:32 -0700

    Betty Doc added

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/*
+ * main - prints the mane of a progam
+ * argc - number of arguements to main.
+ * argv - a pointer to the strings containing the arguements
+ *
+ * Return: (0).
+ */
 
 int main(int argc, char **argv)
 {
