0a7cc48b2a3a6e66a8aa076188326e2f72e20fea
Victor Efobi <victorefobi1@gmail.com>
2023-04-23 13:11:35 -0700

    Task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc - 1]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks for Arguements to main(args, argv) C project.
