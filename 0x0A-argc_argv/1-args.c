e54db52e7716e949ce2ddd3de6dca0c9b6264f0f
Victor Efobi <victorefobi1@gmail.com>
2023-04-23 13:41:29 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguement
+ * @argc: the number of arguements to main
+ * @argv: a pointer to the string containing the arguements
+ *
+ * Return: (0).
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
