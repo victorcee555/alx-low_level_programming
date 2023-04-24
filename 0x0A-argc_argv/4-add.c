47398ed57241b1caba5f71a362f931308acc5fe5
Victor Efobi <victorefobi1@gmail.com>
2023-04-23 17:08:22 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
  * main - adds postive arguments
@@ -13,34 +15,34 @@
 
 int main(int argc, char **argv)
 {
-    int i, sum = 0;
+	int i, sum = 0;
 
-    if (argc > 1)
-    {
-        for (i = 1; i < argc; i++)
-        {
-            char *ptr = argv[i];
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			char *ptr = argv[i];
 
-            while (*ptr != '\0')
-            {
-                if (!isdigit(*ptr))
-                {
-                    printf("Error\n");
-                    return 1;
-                }
-                ptr++;
-            }
+			while (*ptr != '\0')
+			{
+				if (!isdigit(*ptr))
+				{
+					printf("Error\n");
+					return (1);
+				}
+				ptr++;
+			}
 
-            sum += atoi(argv[i]);
-        }
+			sum += atoi(argv[i]);
+		}
 
-        printf("%d\n", sum);
-    }
-    else
-    {
-        printf("%d\n", 0);
-    }
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("%d\n", 0);
+	}
 
-    return 0;
+	return (0);
 }
 
