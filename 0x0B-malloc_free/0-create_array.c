ae1736c0b63e7cfc3c1272130c56e7e01e8f7813
Victor Efobi <victorefobi1@gmail.com>
2023-04-28 16:09:12 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,10 @@ char *create_array(unsigned int size, char c)
 	char *a;
 	int i;
 
+	if (size == 0)
+	{
+		return NULL;
+	}
 	a = malloc(sizeof(char) * size);
 
 	for (i = 0; size > 0; i++, size--)
