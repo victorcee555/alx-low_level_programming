d0aff07cdaa07ef454c383202c6bfc2d248c5ec3
Victor Efobi <victorefobi1@gmail.com>
2023-04-28 16:40:38 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_strdup - duplicates a string.
+ * @str: string to duplicate.
+ *
+ * Return: a pounter to the new string.
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = strlen(str);
+
+	s = malloc(sizeof(char) * i);
+	if (s == NULL)
+		return (NULL);
+	strcpy(str, s);
+
+	return (s);
+}
