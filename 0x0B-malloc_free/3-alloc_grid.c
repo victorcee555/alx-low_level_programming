f80506ec84eb13a2fd1a8b0bb033806337a5706a
Victor Efobi <victorefobi1@gmail.com>
2023-05-01 04:45:07 -0700

    Betty fixed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,39 +13,38 @@
  */
 int **alloc_grid(int width, int height)
 {
-    int i, j;
-    int **arr;
+	int i, j;
+	int **arr;
 
-    if (width <= 0 || height <= 0)
-    {
-        return NULL;
-    }
-
-    arr = malloc(sizeof(int *) * height);
-    if (arr == NULL)
-    {
-        return (NULL);
-    }
-
-    for (i = 0; i < height; i++)
-    {
-        arr[i] = malloc(sizeof(int) * width);
-        if (arr[i] == NULL)
+	if (width <= 0 || height <= 0)
 	{
-            for (j = 0; j < i; j++)
-	    {
-                free(arr[j]);
-            }
-            free(arr);
-            return (NULL);
-        }
+		return (NULL);
+	}
 
-        for (j = 0; j < width; j++)
+	arr = malloc(sizeof(int *) * height);
+	if (arr == NULL)
 	{
-            arr[i][j] = 0;
-        }
-    }
+		return (NULL);
+	}
 
-    return (arr);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(arr[j]);
+			}
+			free(arr);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return (arr);
 }
-
