904c2a808edcbc5229f5714fc21228eeec8bc174
Victor Efobi <victorefobi1@gmail.com>
2023-05-21 14:02:17 -0700

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_binary - converts decimal to binary.
+* @n: an unsifned integer that holds binary value.
+*
+* Return: void
+*/
+
+void print_binary(unsigned long int n)
+{
+	int mask = 1;
+	int check = 0;
+	
+	while (n > 0)
+	{
+		check = n & mask;
+		if (check)
+			_putchar(1 + '0' );
+		if (!check)
+			_putchar(0 + '0');
+		check = 0;
+		n >>= 1;
+	}
+	_putchar('\n');
+}
