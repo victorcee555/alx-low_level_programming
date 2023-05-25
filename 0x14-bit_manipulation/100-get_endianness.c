50c76ed0c096d34bf5439b2addf3ae15184b05f7
Victor Efobi <victorefobi1@gmail.com>
2023-05-25 07:49:14 -0700

    Advance Task 1

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_endianness - a function that checks endianess.
+ *
+ * Return: 0 if big endian, 1 if small endian.
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *a;
+
+	a = (char *) & i;
+
+	if (*a == 1)
+		return (1);
+	else
+		return (0);
+}
