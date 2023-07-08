7fe9d6c31869e457eee56154ee7a8eb68e536d8d
Victor Efobi <victorefobi1@gmail.com>
2023-07-08 04:15:09 -0700

    Task 3 - set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,7 +5,7 @@
  * set_bit - A function that sets the value of
  *           a bit to 1 at given index.
  *
- * @*n: a pointer to an int storing the bits.
+ * @n: a pointer to an int storing the bits.
  * @index: the int storing the index value.
  *
  * Return: 1 if worked, or -1 if an error occured.
