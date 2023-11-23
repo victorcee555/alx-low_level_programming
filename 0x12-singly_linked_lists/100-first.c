a9a2aee6fff05dd6c952a5112df0c5b55ab3bbcf
Victor Efobi <victorefobi1@gmail.com>
2023-11-23 10:14:46 -0800

    Advanced Task 1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,7 +8,7 @@
  */
 
 
-void printMain(void) __attribute__ ((constructor))
+void printMain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
