6136ac6f11f6018b2ef611d830612b492266c666
Victor Efobi <victorefobi1@gmail.com>
2023-11-23 08:06:51 -0800

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,11 +15,12 @@ size_t print_list(const list_t *h)
 	size_t num;
 	
 	num = 0;
-	while (h != NULL || *h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		num++;
 	}
