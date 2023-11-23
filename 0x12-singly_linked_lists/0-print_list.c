b452d67a8b1556fa9570d790b9a3a768c1926c63
Victor Efobi <victorefobi1@gmail.com>
2023-11-23 16:51:08 +0100

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * size_t - A function.
+ * Description: A function that prints
+ * all the elements of a list_t list.
+ *
+ * @h: a node that'll hold the value of the head node.
+ *
+ * Return: number of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t num;
+
+	num = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		num++;
+	}
+
+	return (num);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory contains all the tasks for the 0x12-singly_linked_list
