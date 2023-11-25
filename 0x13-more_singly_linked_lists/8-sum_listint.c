e79c7f546ed9f5f54eb2a2921a1992b19b46c992
Victor Efobi <victorefobi1@gmail.com>
2023-11-25 15:53:55 -0800

    Task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -6,12 +6,12 @@
 int sum_listint(listint_t *head)
 {
 	int sum;
-	
+
 	if (head == NULL)
 		return (0);
 
 	sum = 0;
-	while(head != NULL)
+	while (head != NULL)
 	{
 		sum = sum + head->n;
 		head = head->next;
