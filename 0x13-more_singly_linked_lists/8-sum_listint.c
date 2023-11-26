43ca100dbbbd9088e32472cd4a0f838a2d86770e
Victor Efobi <victorefobi1@gmail.com>
2023-11-25 16:00:18 -0800

    Task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 
 /**
+ * sum_listint - a function that adds all the
+ *               data of a listint_t linked list.
+ *
+ * @head: a node pointing to the head of a list.
+ *
+ * Return: the sum of all the data in the list.
  */
 
 int sum_listint(listint_t *head)
