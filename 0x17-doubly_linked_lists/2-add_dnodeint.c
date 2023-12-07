a594af8655c3a598342a27006b877eae6b84a3fc
Victor Efobi <victorefobi1@gmail.com>
2023-12-07 05:56:34 -0800

    Task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,9 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode;
+	
+	if (head == NULL)
+		return (NULL);
 
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
