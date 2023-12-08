6c302302ed66f7f723596aac916624ced5c277db
Victor Efobi <victorefobi1@gmail.com>
2023-12-07 16:20:48 -0800

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		newNode->next = *h;
-		*h->prev = newNode;
+		(*h)->prev = newNode;
 		*h = newNode;
 
 		return (newNode);
