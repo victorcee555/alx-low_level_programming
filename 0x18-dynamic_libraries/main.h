117a8f10e5d3eab7a9f1c36a4852020a4c51a014
Victor Efobi <victorefobi1@gmail.com>
2023-12-11 06:33:02 -0800

    Header File added

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,10 @@
-#ifndef __MAIN__
-#define __MAIN__
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
 
 int _putchar(char c);
 int _islower(int c);
